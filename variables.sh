b170dc6069f9a18b5c9cee075f270ab4ebbec081
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-12-29 20:36:57 +0530

    Special Variables

--- variables.sh
+++ variables.sh
@@ -11,10 +11,10 @@ echo $dates1
 
 # Special Variables in Shell
 
-echo Script Name $0
-echo First Argument $1
-echo Second Argument $2
-echo All Arguments $*
-echo All arguments2 $@
-echo number of arguments $#
+echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Arguments - $*
+echo All arguments2 - $@
+echo number of arguments - $#
 
